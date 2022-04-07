08d52deaa2c0609bf4560706c31d4605ddcb0732
Jean Luc Houedanou <jeanluc@houedanou.com>
2022-04-07 13:17:00 +0000

    Mise à jour visite virtuelle hd/3D

--- src/app/features/notre-offre/notre-offre.component.ts
+++ src/app/features/notre-offre/notre-offre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-notre-offre',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotreOffreComponent implements OnInit {
 
-  constructor() { }
+  lienvisiteVirtuelle3D!: SafeResourceUrl;
+  lienvisiteVirtuelleHD!: SafeResourceUrl;
+  panoramaAvantRetouche!:string;
+  panoramaApresRetouche!:string;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.lienvisiteVirtuelle3D = this.sanitizer.bypassSecurityTrustResourceUrl("https://my.matterport.com/show/?m=W9ufV4AFQs6");
+    this.lienvisiteVirtuelleHD = this.sanitizer.bypassSecurityTrustResourceUrl("https://webobook.com/public/61d31c5b04cc4d646862bf92,en?ap=true&si=true&sm=false&sp=true&sfr=false&sl=false&sop=false&");
+  }
 
   ngOnInit(): void {
+
+    this.panoramaAvantRetouche = "./assets/images/panoBefore.webp";
+    this.panoramaApresRetouche = "./assets/images/panoAfter.webp";
+    //comparaison
+
   }
 
 }
