2bd5f754c204f65bcd36042ab956b68d1c8efee8
KOUAME Ulrich <ulrich@bigfiveabidjan.com>
2022-05-04 12:09:41 +0000

    Update transte

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from '../core/services/auth.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
+//useClass: AuthInterceptor,
